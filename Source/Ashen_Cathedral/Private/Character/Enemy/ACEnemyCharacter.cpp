f8ca44690a26cd799abcfb9685c4600516460be8
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2026-04-04 09:35:31 +0900

    refactor(Character): 비동기 데이터 로드 및 초기화 로직 추가
    - `PossessedBy` 함수에 비동기 데이터 로드 로직 구현
    - `UAssetManager`를 사용해 `CharacterStartUpData`를 비동기로 로드
    - 로드된 데이터로 Ability System Component에 초기 설정 적용
    - 기존의 동기 처리 방식에서 비동기 방식으로 전환하여 성능 최적화

--- Source/Ashen_Cathedral/Private/Character/Enemy/ACEnemyCharacter.cpp
+++ Source/Ashen_Cathedral/Private/Character/Enemy/ACEnemyCharacter.cpp
@@ -3,7 +3,9 @@
 
 #include "Character/Enemy/ACEnemyCharacter.h"
 #include "Controllers/ACEnemyController.h"
+#include "DataAssets/Startup/ACDataAsset_EnemyStartupData.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Engine/AssetManager.h"
 
 AACEnemyCharacter::AACEnemyCharacter()
 {
@@ -23,3 +25,26 @@ UPawnCombatComponent* AACEnemyCharacter::GetPawnCombatComponent() const
 	return EnemyCombatComponent;
 }
 
+void AACEnemyCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	if (CharacterStartUpData.IsNull())
+	{
+		return;
+	}
+
+	int32 AbilityApplyLevel = 1;
+
+	UAssetManager::GetStreamableManager().RequestAsyncLoad(
+		CharacterStartUpData.ToSoftObjectPath(),
+		FStreamableDelegate::CreateLambda(
+			[this,AbilityApplyLevel]() {
+				if (UACDataAsset_EnemyStartupData* LoadedData = Cast<UACDataAsset_EnemyStartupData>(CharacterStartUpData.Get()))
+				{
+					LoadedData->GiveToAbilitySystemComponent(ACAbilitySystemComponent, AbilityApplyLevel);
+				}
+			}
+			)
+		);
+}
