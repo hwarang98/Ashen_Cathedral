2a8e87f7f0d6a0b765bbb0efd9cd2e419ace1a16
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2026-04-08 18:23:15 +0900

    feat(Player): 히트리액트 방향별 애니메이션 및 효과 추가
    - 공격 방향에 따라 다른 히트리액트 몽타주(정면, 좌측, 우측, 후면) 재생 로직 구현
    - 블록 중 피격 시 전용 블록 히트리액트 몽타주 지원
    - 공격 방향 계산을 위한 `UACFunctionLibrary::ComputeHitReactDirectionTag` 사용
    - 히트리액트 재생 중 카메라 셰이크 및 상태 효과 적용
    - 몽타주 종료 및 취소 처리 로직 추가
    
    플레이어 피격 반응의 방향성을 강화해 전투의 몰입감을 높임.

--- Source/Ashen_Cathedral/Private/GameplayAbilitySystem/Abilities/Player/ACPlayerGameplayAbility_HitReact.cpp
+++ Source/Ashen_Cathedral/Private/GameplayAbilitySystem/Abilities/Player/ACPlayerGameplayAbility_HitReact.cpp
@@ -1,12 +1,21 @@
-// 플레이어가 피격되었을 때 카메라 셰이크를 재생하는 히트리액트 어빌리티
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "GameplayAbilitySystem/Abilities/Player/ACPlayerGameplayAbility_HitReact.h"
+#include "ACFunctionLibrary.h"
 #include "ACGameplayTags.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 #include "Camera/CameraShakeBase.h"
 #include "GameFramework/PlayerController.h"
+#include "GameplayAbilitySystem/ACAbilitySystemComponent.h"
 
 UACPlayerGameplayAbility_HitReact::UACPlayerGameplayAbility_HitReact()
 {
+	FGameplayTagContainer TagsToAdd;
+	TagsToAdd.AddTag(ACGameplayTags::Shared_Ability_HitReact);
+	SetAssetTags(TagsToAdd);
+
+	ActivationOwnedTags.AddTag(ACGameplayTags::Shared_Status_HitReact);
+
 	FAbilityTriggerData TriggerData;
 	TriggerData.TriggerTag = ACGameplayTags::Shared_Event_HitReact;
 	TriggerData.TriggerSource = EGameplayAbilityTriggerSource::GameplayEvent;
@@ -21,6 +30,32 @@ void UACPlayerGameplayAbility_HitReact::ActivateAbility(
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
+	const AActor* InstigatorActor = TriggerEventData ? TriggerEventData->Instigator.Get() : nullptr;
+	AActor* AvatarActor = GetAvatarActorFromActorInfo();
+
+	const bool bIsBlocking = GetACAbilitySystemComponentFromActorInfo()->HasMatchingGameplayTag(ACGameplayTags::Player_Status_Blocking);
+
+	UAnimMontage* MontageToPlay = bIsBlocking ? BlockHitReactMontage : FrontHitReactMontage;
+
+	if (InstigatorActor && AvatarActor)
+	{
+		float AngleDifference = 0.f;
+		const FGameplayTag HitDirectionTag = UACFunctionLibrary::ComputeHitReactDirectionTag(InstigatorActor, AvatarActor, AngleDifference);
+
+		if (HitDirectionTag.MatchesTagExact(ACGameplayTags::Shared_Status_HitReact_Left))
+		{
+			MontageToPlay = bIsBlocking ? BlockHitReactMontage : LeftHitReactMontage;
+		}
+		else if (HitDirectionTag.MatchesTagExact(ACGameplayTags::Shared_Status_HitReact_Right))
+		{
+			MontageToPlay = bIsBlocking ? BlockHitReactMontage : RightHitReactMontage;
+		}
+		else if (HitDirectionTag.MatchesTagExact(ACGameplayTags::Shared_Status_HitReact_Back))
+		{
+			MontageToPlay = bIsBlocking ? BlockHitReactMontage : BackHitReactMontage;
+		}
+	}
+
 	if (HitCameraShakeClass)
 	{
 		if (APlayerController* PlayerController = Cast<APlayerController>(ActorInfo->PlayerController.Get()))
@@ -29,5 +64,39 @@ void UACPlayerGameplayAbility_HitReact::ActivateAbility(
 		}
 	}
 
-	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	if (MontageToPlay)
+	{
+		UAbilityTask_PlayMontageAndWait* MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+			this,
+			NAME_None,
+			MontageToPlay,
+			1.0f,
+			NAME_None,
+			false,
+			1.0f);
+
+		MontageTask->OnCompleted.AddDynamic(this, &ThisClass::OnMontageEnded);
+		MontageTask->OnBlendOut.AddDynamic(this, &ThisClass::OnMontageEnded);
+		MontageTask->OnInterrupted.AddDynamic(this, &ThisClass::OnMontageCancelled);
+		MontageTask->OnCancelled.AddDynamic(this, &ThisClass::OnMontageCancelled);
+
+		ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, MakeOutgoingGameplayEffectSpec(UnderAttackEffect, 1));
+
+		MontageTask->ReadyForActivation();
+	}
+	else
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	}
+}
+
+void UACPlayerGameplayAbility_HitReact::OnMontageEnded()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UACPlayerGameplayAbility_HitReact::OnMontageCancelled()
+{
+
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
 }
--- Source/Ashen_Cathedral/Public/GameplayAbilitySystem/Abilities/Player/ACPlayerGameplayAbility_HitReact.h
+++ Source/Ashen_Cathedral/Public/GameplayAbilitySystem/Abilities/Player/ACPlayerGameplayAbility_HitReact.h
@@ -1,4 +1,4 @@
-// 플레이어가 피격되었을 때 카메라 셰이크를 재생하는 히트리액트 어빌리티
+// 플레이어가 피격되었을 때 방향별 몽타주와 카메라 셰이크를 재생하는 히트리액트 어빌리티
 
 #pragma once
 
@@ -7,9 +7,10 @@
 #include "ACPlayerGameplayAbility_HitReact.generated.h"
 
 class UCameraShakeBase;
+class UGameplayEffect;
 
 /**
- * @brief Shared_Event_HitReact 이벤트로 트리거되며, 플레이어 카메라 셰이크를 재생한다.
+ * @brief Shared_Event_HitReact 이벤트로 트리거되며, 공격 방향에 따른 몽타주와 카메라 셰이크를 재생한다.
  */
 UCLASS()
 class ASHEN_CATHEDRAL_API UACPlayerGameplayAbility_HitReact : public UACPlayerGameplayAbility
@@ -22,7 +23,31 @@ public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
 private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage|HitReact", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAnimMontage> FrontHitReactMontage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage|HitReact", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAnimMontage> LeftHitReactMontage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage|HitReact", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAnimMontage> RightHitReactMontage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage|HitReact", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAnimMontage> BackHitReactMontage;
+
+	// 블록 중 피격 시 방향별 몽타주
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage|Block", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAnimMontage> BlockHitReactMontage;
+
+	TSubclassOf<UGameplayEffect> UnderAttackEffect;
+
 	/** 피격 시 재생할 카메라 셰이크 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CameraShake", meta = (AllowPrivateAccess = "true"))
 	TSubclassOf<UCameraShakeBase> HitCameraShakeClass;
+
+	UFUNCTION()
+	void OnMontageEnded();
+
+	UFUNCTION()
+	void OnMontageCancelled();
 };
