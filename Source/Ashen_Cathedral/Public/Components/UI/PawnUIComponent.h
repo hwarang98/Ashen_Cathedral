6eb9153e3e225bf391c2a32d0dc477881368cfcc
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2026-04-13 04:03:51 +0900

    feat(UI): Pawn 및 Player/Enemy UI 컴포넌트 및 인터페이스 초기 구현
    PawnUIComponent를 기반으로 한 PlayerUIComponent 및 EnemyUIComponent 클래스 추가.
    Pawn UI 관련 공통 동작 정의를 위한 PawnUIInterface 생성.
    
    - `UPawnUIComponent`: 공통 UI 로직을 처리하기 위한 컴포넌트 클래스 생성
    - `UPlayerUIComponent`, `UEnemyUIComponent`: 플레이어와 적 전용 UI 컴포넌트 클래스 구현
    - `IPawnUIInterface`: Pawn UI 컴포넌트와의 상호작용을 위한 인터페이스 추가
    
    UI 모듈의 확장성을 고려한 컴포넌트 구조 초기화.

---
+++ Source/Ashen_Cathedral/Public/Components/UI/PawnUIComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PawnExtensionComponentBase.h"
+#include "PawnUIComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASHEN_CATHEDRAL_API UPawnUIComponent : public UPawnExtensionComponentBase
+{
+	GENERATED_BODY()
+	
+};
