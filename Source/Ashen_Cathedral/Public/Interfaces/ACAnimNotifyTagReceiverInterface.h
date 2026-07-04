7ba556482ded2d9f7d11ba58fd0c2d59c0c14041
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2026-07-04 19:07:04 +0900

    feat(Animation): GameplayTag 전달 인터페이스 추가
    - AnimNotify가 부여/제거하는 GameplayTag를 전달받기 위한 `IACAnimNotifyTagReceiverInterface` 정의
      - `OnAnimNotifyAddGameplayTags`, `OnAnimNotifyRemoveGameplayTags` 함수 추가 및 기본 구현 작성
    - 프로젝트 구조 개선: 인터페이스 관련 .h/.cpp 파일 생성 및 정리
    
    GameplayTag 기반 애니메이션 시스템 확장을 위한 기초 작업.

---
+++ Source/Ashen_Cathedral/Public/Interfaces/ACAnimNotifyTagReceiverInterface.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "UObject/Interface.h"
+#include "ACAnimNotifyTagReceiverInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(BlueprintType)
+class UACAnimNotifyTagReceiverInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * ANS_AddGameplayTag가 몽타주 구간 동안 부여/제거하는 GameplayTag를 전달받는 인터페이스
+ */
+class ASHEN_CATHEDRAL_API IACAnimNotifyTagReceiverInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, Category = "AnimNotify|GameplayTag")
+	void OnAnimNotifyAddGameplayTags(const FGameplayTagContainer& GameplayTags);
+	virtual void OnAnimNotifyAddGameplayTags_Implementation(const FGameplayTagContainer& GameplayTags)
+	{
+	}
+
+	UFUNCTION(BlueprintNativeEvent, Category = "AnimNotify|GameplayTag")
+	void OnAnimNotifyRemoveGameplayTags(const FGameplayTagContainer& GameplayTags);
+	virtual void OnAnimNotifyRemoveGameplayTags_Implementation(const FGameplayTagContainer& GameplayTags)
+	{
+	}
+};
