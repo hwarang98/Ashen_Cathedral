956a78ce1b811b60d894e8959b68349498f8f8fa
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2026-07-11 14:26:47 +0900

    feat(MetaProgression): 보스 세이브 키용 GameplayTag 추가
    메타 성장 재화 시스템에서 보스별 첫 클리어 여부를 판정할 식별자로
    쓸 MetaProgression.BossID.* 네이티브 태그를 전투용 태그와 별도 파일로 분리해 추가.

---
+++ Source/Ashen_Cathedral/Private/GameplayTags/ACGameplayTags_MetaProgression.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameplayTags/ACGameplayTags_MetaProgression.h"
+
+namespace ACGameplayTags
+{
+	#pragma region MetaProgression BossID Tags
+	UE_DEFINE_GAMEPLAY_TAG(MetaProgression_BossID_AshenKnight, "MetaProgression.BossID.AshenKnight")
+	#pragma endregion
+}
